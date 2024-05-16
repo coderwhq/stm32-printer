c8dfc87a56271a87a01ffcb1a06afe6f42b20775
Captain <2622189698@qq.com>
2024-05-16 22:49:25 +0800

    更改为使用消息队列向外界传递串口接收到的数据

--- software/Hardware/Serial.h
+++ software/Hardware/Serial.h
@@ -2,11 +2,10 @@
 #define __SERIAL_H
 #include "stm32f10x.h"
 
-extern char serialReceiveString[100];
+extern uint8_t serialReceiveString[];
 
 void Serial_Init(uint32_t baudRate);
 void Serial_SendByte(uint8_t byte);
 void Serial_SendString(char *str);
-uint8_t Serial_GetReceiveFlag(void);
 
 #endif
